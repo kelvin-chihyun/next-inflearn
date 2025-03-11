f79702fa257786dcfce710cb87f3ae2d0376ecc3
kelvin-chihyun <shclgus111@gmail.com>
2025-03-12 00:29:13 +0900

    chore: sonner 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,3 +2,4 @@ export * from "@material-tailwind/react";
 
 export { Button as ShadButton } from "./button";
 export { Input as ShadInput } from "./input";
+export { Toaster as SonnerToaster } from "./sonner";
